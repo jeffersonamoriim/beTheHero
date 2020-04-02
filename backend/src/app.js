2772e379d321dce5681e9218f452bd65876aa31d
Jefferson Amorim <37017888+jeffersonamoriim@users.noreply.github.com>
2020-04-02 12:01:49 -0300

    Implemented tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate')
 const routes = require('./routes');
 
 const app = express();
@@ -10,7 +11,9 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333);
+app.use(errors());
+
+module.exports = app;
 
 /*
     Params Types:
